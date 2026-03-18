f31ad344c6c8b388c95f05b386ccefb8b3275260
Jun <tjrwns0012@naver.com>
2026-03-18 16:01:52 +0900

    d

--- main.cpp
+++ main.cpp
@@ -2,61 +2,8 @@
 
 using namespace std;
 
-class FSM
-{
-	FSM();
-	~FSM();
-
-	UWorld* World;
-	UActor* Actor;
-	UMove* Move;
-
-	void GameOver();
-};
-
-class UWorld
-{
-	UWorld();
-	~UWorld();
-
-	unsigned int Wall;
-	int Plane;
-	int SavePoint;
-
-	bool isIt;
-};
-
-class UActor
-{
-	UActor();
-	~UActor();
-
-	int Player;
-	int Monster;
-};
-
-class UMove
-{
-	UMove();
-	~UMove();
-
-	int Fword;
-	int Right;
-
-	bool isMove;
-};
-
 int main()
 {
 
 	return 0;
-}
-
-void FSM::GameOver()
-{
-	
-	if (SavePont == Player)
-	{
-		cout << "GameOver";
-	}
 }
